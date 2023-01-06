ef0f77a2508942751f7daf0a928d658c17eab9f3
Nicepops1 <91388580+Nicepops1@users.noreply.github.com>
2023-01-06 13:31:50 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/cataloguerforbooks/ExportActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cataloguerforbooks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_export)
+    }
+}
