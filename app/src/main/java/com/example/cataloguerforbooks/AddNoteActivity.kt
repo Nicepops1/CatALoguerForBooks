757418ecc2147d4ffb88d6ed17e380f0f8acd049
Nicepops1 <91388580+Nicepops1@users.noreply.github.com>
2023-01-09 14:07:06 +0300

    fetch: set addnote menu, normalize color in plainText

--- app/src/main/java/com/example/cataloguerforbooks/AddNoteActivity.kt
+++ app/src/main/java/com/example/cataloguerforbooks/AddNoteActivity.kt
@@ -2,10 +2,16 @@ package com.example.cataloguerforbooks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class AddNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu) : Boolean {
+        getMenuInflater().inflate(R.menu.addnote, menu)
+        return true
     }
 }
